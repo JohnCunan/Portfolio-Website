d29322d9724a57b6ba37c06605256a281188f7af
Ulysses John Cunan <cunan.102469@angeles.sti.edu.ph>
2023-02-15 14:17:25 +0800

    Added loading icon for cert

--- src/components/CertificationsPage.jsx
+++ src/components/CertificationsPage.jsx
@@ -1,23 +1,31 @@
 import '../styles/CertificationsPage.css'
+import LoadingIcon from '../material_ui_components/loading_icon'
+import { useState } from 'react'
 
+// Images of certificates
 import cert1 from "../images/c1.png"
 import cert2 from "../images/c2.png"
 import cert3 from "../images/c3.png"
 import cert4 from "../images/c4.png"
 import cert5 from "../images/c5.png"
 
-
 export default function CertificationsPage() {
+
+  const [loaded, setLoaded] = useState(false);
+
+  const handleImageLoad = () => { setLoaded(true) }
+
   return (
     <div className='CertificationsPageContainer'>
         <h2>Certifications</h2>
         <p>Certifications I earned throughout the years</p>
         <div className='CertsContainer'>
-            <img src={cert1}></img>
-            <img src={cert2}></img>
-            <img src={cert3}></img>
-            <img src={cert4}></img>
-            <img src={cert5}></img>
+        {!loaded && <LoadingIcon />}
+          <img src={cert1} onLoad={handleImageLoad} style={loaded ? {} : { display: 'none' }} />
+          <img src={cert2} onLoad={handleImageLoad} style={loaded ? {} : { display: 'none' }} />
+          <img src={cert3} onLoad={handleImageLoad} style={loaded ? {} : { display: 'none' }} />
+          <img src={cert4} onLoad={handleImageLoad} style={loaded ? {} : { display: 'none' }} />
+          <img src={cert5} onLoad={handleImageLoad} style={loaded ? {} : { display: 'none' }} />
         </div>
     </div>
   )
