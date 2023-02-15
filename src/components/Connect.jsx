0287061c1eb24f7ec5abaa2e7ccb2a1c98aeb2db
Ulysses John Cunan <cunan.102469@angeles.sti.edu.ph>
2023-02-15 14:01:44 +0800

    Added loading on send message

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,18 +1,28 @@
 import '../styles/Connect.css'
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import LoadingIcon from '../material_ui_components/loading_icon';
 
 export default function Connect() {
 
     const form = useRef();
-  
+
+    const [showLoading, setShowLoading] = useState(false);
+    const [showSuccessSend, setShowSuccessSend] = useState(false);
+
+    const handleModal = () => {
+        setShowSuccessSend(false);
+    };
+
     const sendEmail = (e) => {
       e.preventDefault();
+      setShowLoading(true)
   
       emailjs.sendForm('service_gjjvni8', 'template_2xal5ua', form.current, 'HGl6qsDZH-bh_lr82')
         .then((result) => {
             console.log(result.text);
-            alert("Your message was successfully sent!");
+            setShowLoading(false);
+            setShowSuccessSend(true);
         }, (error) => {
             console.log(error.text);
             alert("Error in sending your message");
@@ -29,9 +39,21 @@ export default function Connect() {
                     <input type="text" name="user_name" placeholder='Enter your name (optional)'/>
                     <label>Message</label>
                     <textarea name="message" placeholder="Enter your message here..." />
-                    <input type="submit" value="SEND" />
+                    <input type="submit" value="SEND"/>
+                    {showLoading && 
+                        <div className='LoadingContainer'>
+                            <p>Sending Your Message...</p>
+                            <LoadingIcon className='LoadingIcon'/>
+                        </div>
+                    }
                 </form>
             </div>
+            {showSuccessSend && 
+                <div className='MessageModalContainer'>
+                    <h4>Your Message Was Successfully sent!</h4>
+                    <button onClick={handleModal}>OK</button>
+                </div>
+            }
         </div>
     )
 }
