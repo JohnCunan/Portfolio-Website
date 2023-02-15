44525f320df77299f5c17089605af09992c58d73
Ulysses John Cunan <cunan.102469@angeles.sti.edu.ph>
2023-02-15 10:31:47 +0800

    Resume Button Added

--- src/components/MyInfo.jsx
+++ src/components/MyInfo.jsx
@@ -1,5 +1,6 @@
 import '../styles/MyInfo.css'
 import ProfilePic from '../../public/profile_pic.png'
+import { Link } from 'react-router-dom';
 
 export default function MyInfo() {
   return (
@@ -8,7 +9,7 @@ export default function MyInfo() {
             <h1>ULYSSES JOHN CUNAN</h1>
             <h2>Front-End Developer</h2>
             <p>I'm a 4th year BSIT College student with a love for learning new things and making websites look awesome.</p>
-            {/* <h4>VIEW MY RESUME</h4> */}
+            <h4><a href='/public/resume.pdf' target={'_blank'}>VIEW MY RESUME</a></h4>
         </div>
         <img src={ProfilePic} />
     </div>
