6f86293bf7f228dfee249ef983e8927e463c48e1
Ulysses John Cunan <cunan.102469@angeles.sti.edu.ph>
2023-02-15 20:30:03 +0800

    Icon Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,13 @@ import ProjectsPage from './components/ProjectsPage'
 import CertificationsPage from './components/CertificationsPage'
 import { useState } from 'react'
 
+// Material UI
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+
 export default function App() {
 
   const [activeSection, setActiveSection] = useState("home");
@@ -15,6 +22,19 @@ export default function App() {
   const handleSectionChange = section => {
       window.scrollTo(0,0);
       setActiveSection(section)
+  };
+
+  // Nav Menu Functions
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
@@ -26,6 +46,32 @@ export default function App() {
           <li><a href="#" onClick={() => handleSectionChange("certs")}>Certifications</a></li>
           <li><a href="#" onClick={() => handleSectionChange("contacts")}>Contact</a></li>
         </ul>
+        <div className='NavMenu'>
+            <Button
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+            >
+              <MenuIcon />
+            </Button>
+
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={() => handleSectionChange("home")}>Home</MenuItem>
+              <MenuItem onClick={() => handleSectionChange("projects")}>Projects</MenuItem>
+              <MenuItem onClick={() => handleSectionChange("certs")}>Certifications</MenuItem>
+              <MenuItem onClick={() => handleSectionChange("contacts")}>Contact</MenuItem>
+            </Menu>
+          </div>
       </nav>
       <main>
         {activeSection === "home" && (
@@ -47,8 +93,9 @@ export default function App() {
           <div className='ConnectSection' style={{ marginTop: "60px" }}><Connect /></div>
         )}
       </main>
-      <footer>
+      <footer className='MainFooterContainer'>
         <Footer />
+        <p>Made with 🔥 by Ulysses John Cunan</p>
       </footer>
     </div>
   )
--- src/components/MyInfo.jsx
+++ src/components/MyInfo.jsx
@@ -7,7 +7,11 @@ export default function MyInfo() {
         <div className='PersonalInfo'>
             <h1>ULYSSES JOHN CUNAN</h1>
             <h2>Front-End Developer</h2>
-            <p>I'm a 4th year BSIT College student with a love for learning new things and making websites look awesome.</p>
+            <p>
+              I'm a 4th year BSIT College student that is eager to learn and collaboorate 
+              with a love for learning new things 
+              and making websites look awesome.
+            </p>
             <h4>
               <a 
                 href='https://drive.google.com/file/d/1rlbcjeW1yk5xE3AL7f5e-udMBmp2Zt1x/view?usp=sharing' 
